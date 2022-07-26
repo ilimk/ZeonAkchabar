08479ec975ef056f1f75b5b40bdbee774412733f
ilimk <ikalmanbetov@gmail.com>
2022-07-26 21:01:32 +0600

    second commit for Akchabar

--- ParserAkchabar.py
+++ ParserAkchabar.py
@@ -3,23 +3,19 @@ import json
 from bs4 import BeautifulSoup
 import csv
 import datetime
-# AkchabarUrl = 'https://www.akchabar.kg/ru/exchange-rates/'
-#
-# headersForAkchabar = {
-#     "Accept": "*/*",
-#     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
-# }
-#
-# reqAkchabar = requests.get(AkchabarUrl, headers = headersForAkchabar)
-# srcAkchabar = reqAkchabar.text
+AkchabarUrl = 'https://www.akchabar.kg/ru/exchange-rates/'
 
-# with open('index.html', 'w') as file:
-#     file.write(srcAkchabar)
-with open('index.html') as file:
-    src = file.read()
+headersForAkchabar = {
+    "Accept": "*/*",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
+}
 
-soup = BeautifulSoup(src, 'lxml')
-#tablesorter table-hover hidden-xs tablesorter-default
+reqAkchabar = requests.get(AkchabarUrl, headers = headersForAkchabar)
+srcAkchabar = reqAkchabar.text
+
+
+soup = BeautifulSoup(srcAkchabar, 'lxml')
+
 all_rates = soup.find("div", {"id": "the-content"}) #Находим div у котого id = the_content
 
 all_rates_string =  all_rates.script.text
